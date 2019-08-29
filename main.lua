90204d2521ab0458351381747c3617da83d20896
Ro7Rinke <ro7rinke@gmail.com>
2019-08-29 20:49:59 -0300

    background loop scroll

--- main.lua
+++ main.lua
@@ -1,38 +1,10 @@
 function love.load()
 
-    background = {}
-    background.image = love.graphics.newImage('assets/images/nurse_walk_1.png')
-    local obj = {}
-    obj.source = background.image
-    obj.x = 0
-    obj.y = 0
-    background.images_alive = {}
-    table.insert(background.images_alive, obj)
-    obj.source = love.graphics.newImage('assets/images/nurse_walk_1.png')
-    obj.x = 800
-    obj.y = 0
-    table.insert(background.images_alive, obj)
-    background.speed = 1
-    background.draw = function ()
-            love.graphics.draw(background.images_alive[1].source, background.images_alive[1].x, background.images_alive[1].y)
-            love.graphics.draw(background.images_alive[2].source, background.images_alive[2].x, background.images_alive[2].y)
-    end
-    background.update = function (dt)
-        if background.images_alive[1].x + background.images_alive[1].source:getWidth() < 0 then
-            background.images_alive[1].x = 0
-        end
-        if background.images_alive[2].x + background.images_alive[2].source:getWidth() < 800 then
-            background.images_alive[2].x = 0
-        end
-        background.images_alive[1].x = background.images_alive[1].x + (dt * 10)
-        background.images_alive[2].x = background.images_alive[2].x + (dt * 10)
-    end
-
     Object = require 'src/classic'
     require 'src/player'
     require 'src/enemy'
     require 'src/scoreboard'
-    -- require 'src/background'
+    require 'src/background'
     player = Player()
     enemies = {Enemy('zombie')}
     scoreboard = Scoreboard()
@@ -42,19 +14,20 @@ end
 
 function love.update(dt)
 
-    player:update(dt)
-    scoreboard:update(dt)
+    -- player:update(dt)
+    -- scoreboard:update(dt)
     background.update(dt)
-    function love.keypressed(key)
-        verifyKey()
-    end
+    -- function love.keypressed(key)
+    --     verifyKey()
+    -- end
 
 end
 
 function love.draw()
     -- player:draw()
-        love.graphics.draw(background.images_alive[1].source, background.images_alive[1].x, background.images_alive[1].y)
-        love.graphics.draw(background.images_alive[2].source, background.images_alive[2].x, background.images_alive[2].y)
+    -- love.graphics.print(background.images_alive[1].x, 0 ,0)
+    love.graphics.draw(background.images_alive[1].source, background.images_alive[1].x, background.images_alive[1].y)
+    love.graphics.draw(background.images_alive[2].source, background.images_alive[2].x, background.images_alive[2].y)
     -- scoreboard:draw()
 end
 
--- src/background.lua
+++ src/background.lua
@@ -1,27 +1,27 @@
 background = {}
-background.image = love.graphics.newImage('assets/images/nurse_walk_1.png')
-local obj = {}
-obj.source = background.image
-obj.x = 0
-obj.y = 0
-background.images_alive = {}
-table.insert(background.images_alive, obj)
-obj.source = love.graphics.newImage('assets/images/nurse_walk_1.png')
-obj.x = 800
-obj.y = 0
-table.insert(background.images_alive, obj)
-background.speed = 1
-background.draw = function ()
-        love.graphics.draw(background.images_alive[1].source, background.images_alive[1].x, background.images_alive[1].y)
-        love.graphics.draw(background.images_alive[2].source, background.images_alive[2].x, background.images_alive[2].y)
-end
-background.update = function (dt)
-    if background.images_alive[1].x + background.images_alive[1].source:getWidth() < 0 then
-        background.images_alive[1].x = 0
+    background.image = love.graphics.newImage('assets/images/nurse_walk_all.png')
+    local obj = {}
+    obj.source = background.image
+    obj.x = 0
+    obj.y = 0
+    background.images_alive = {}
+    table.insert(background.images_alive, obj)
+    local obj2 = {}
+    obj2.source = love.graphics.newImage('assets/images/nurse_walk_all.png')
+    obj2.x = 800
+    obj2.y = 0
+    table.insert(background.images_alive, obj2)
+    background.speed = 1
+    background.draw = function ()
+            love.graphics.draw(background.images_alive[1].source, background.images_alive[1].x, background.images_alive[1].y)
+            love.graphics.draw(background.images_alive[2].source, background.images_alive[2].x, background.images_alive[2].y)
     end
-    if background.images_alive[2].x + background.images_alive[2].source:getWidth() < 800 then
-        background.images_alive[2].x = 0
+    background.update = function (dt)
+        if background.images_alive[1].x + background.images_alive[1].source:getWidth() < 0 then
+            background.images_alive[1].x = 0
+            background.images_alive[2].x = 800
+        end
+        
+        background.images_alive[1].x = background.images_alive[1].x - (dt * 10)
+        background.images_alive[2].x = background.images_alive[2].x - (dt * 10)
     end
-    background.images_alive[1].x = background.images_alive[1].x + (dt * 10)
-    background.images_alive[2].x = background.images_alive[2].x + (dt * 10)
-end
